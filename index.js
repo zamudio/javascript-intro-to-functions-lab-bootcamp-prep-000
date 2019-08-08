f8126f8a335384491f50a7858b31e239a08c0ace
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-08 01:20:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,21 +22,6 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
-    return ""
+    return "I love you, too."
   }
 }
-
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
