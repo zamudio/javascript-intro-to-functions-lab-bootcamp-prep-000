637f7cc58bc72a9487f270ff54648485e471139d
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-08 01:08:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,51 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout)
+}
+
+function logWhisper(string) {
+  console.log(whisper)
+}
+
+
+//grandma
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = "hello!"
+lowercase.toLowerCase() === lowercase //true
+
+var mixedCase = "Hi there!"
+mixedCase.toLowerCase() === mixedCase //false
+mixedCase.toUpperCase() === mixedCase //false
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
+  }
+  else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+}
+
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
