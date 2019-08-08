dc2b903b81cbebeb1e1c448d4185ca1ec68aafb6
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-08 01:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,17 +16,12 @@ function logWhisper(string) {
 
 
 //grandma
-var uppercase = "HELLO!"
-var lowercase = "hello!"
-var mixedCase = "Hi there!"
-
-
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!"
-  } if (string === string.toUpperCase) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
-  }
+  } else if (string === string)
 }
 
 
